303f46019e1d33c7d2f6e2f84d5f834b2d4ba315
73LIX <gouravydv.exe@gmail.com>
2026-08-28 00:07:37 +0530

    declaration of set instructions

--- chip8.hpp
+++ chip8.hpp
@@ -58,6 +58,18 @@ private:
 	//add Vx = Vx + KK
 	void OP_7XKK();
 
+	//set Vx = Vy
+	void OP_8XY0();
+
+	//set Vx = Vx or Vy
+	void OP_8XY1();
+
+	//set Vx = Vx AND Vy
+	void OP_8XY2();
+
+	//set Vx = Vx XOR Vy
+	void OP_8XY3();
+
     std::default_random_engine randGen;
 	std::uniform_int_distribution<uint8_t> randByte;
 };
