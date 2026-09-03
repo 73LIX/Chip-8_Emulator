0dc085821acdb076d11a484255a03e1643744f4c
73LIX <gouravydv.exe@gmail.com>
2026-09-03 13:02:32 +0530

    update: skip instructions(EX9E and EXA1), set instruction(FX07), wait for keypress and store the value of key in Vx(FX0A)

--- chip8.cpp
+++ chip8.cpp
@@ -304,6 +304,7 @@ void Chip8::OP_DXYN(){
 
         for(unsigned int col = 0; col < 8; ++col){ //each byte contains 8 horizontal pixels
             uint8_t spritePixel = spriteByte & (0x80u >> col);
+            //calc targets array index
             uint32_t* screenPixel = &video[(yPos + row) * VIDEO_WIDTH + (xPos + col)];
 
             if(spritePixel){
@@ -315,5 +316,86 @@ void Chip8::OP_DXYN(){
             }
         }
     }
+}
 
+//skip next instruction if a key with the value of Vx is pressed (registers[Vx] == keypad[key])
+void Chip8::OP_EX9E(){
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t key = registers[Vx];
+
+    if(keypad[key]){
+        pc += 2;
+    }
+}
+
+//skip next instruction if a key with the value of Vx is not pressed
+void Chip8::OP_EXA1(){
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t key = registers[Vx];
+
+    if(keypad[key]){
+        pc += 2;
+    }
+}
+
+//set Vx = delay timer value
+void Chip8::OP_FX07(){
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    registers[Vx] = delayTimer;
+}
+
+//Wait for a key press..., then store the value of the key in Vx
+void Chip8::OP_FX0A(){
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    if(keypad[0]){
+        registers[Vx] = 0;
+    }
+    else if(keypad[1]){
+        registers[Vx] = 1;
+    }
+    else if(keypad[2]){
+        registers[Vx] = 2;
+    }
+    else if(keypad[3]){
+        registers[Vx] = 3;
+    }
+    else if(keypad[4]){
+        registers[Vx] = 4;
+    }
+    else if(keypad[5]){
+        registers[Vx] = 5;
+    }
+    else if(keypad[6]){
+        registers[Vx] = 6;
+    }
+    else if(keypad[7]){
+        registers[Vx] = 7;
+    }
+    else if(keypad[8]){
+        registers[Vx] = 8;
+    }
+    else if(keypad[9]){
+        registers[Vx] = 9;
+    }
+    else if(keypad[10]){
+        registers[Vx] = 10;
+    }
+    else if(keypad[11]){
+        registers[Vx] = 11;
+    }
+    else if(keypad[12]){
+        registers[Vx] = 12;
+    }
+    else if(keypad[13]){
+        registers[Vx] = 13;
+    }
+    else if(keypad[14]){
+        registers[Vx] = 14;
+    }
+    else if(keypad[15]){
+        registers[Vx] = 15;
+    } else {
+        pc -= 2; //by doing this i cancel out the pc+=2 which is to go on the next instruction by the main loop
+    }
 }
