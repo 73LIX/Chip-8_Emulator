d088186b5f1f2be5b11b0541ae111d179445aed4
73LIX <gouravydv.exe@gmail.com>
2026-09-04 18:55:12 +0530

    wip: function pointer table, created entries for table0, table8 and tableE

--- chip8.cpp
+++ chip8.cpp
@@ -68,7 +68,8 @@ Chip8::Chip8():randGen(std::chrono::system_clock::now().time_since_epoch().count
     //initialize rng
     randByte = std::uniform_int_distribution<uint8_t>(0, 255U);
 
-    //function pointer table
+    //function pointer lookup table (jump table)
+    table[0x0] = &Chip8::Table0;
     table[0x1] = &Chip8::OP_1NNN;
     table[0x2] = &Chip8::OP_2NNN;
     table[0x3] = &Chip8::OP_3XKK;
@@ -76,8 +77,59 @@ Chip8::Chip8():randGen(std::chrono::system_clock::now().time_since_epoch().count
     table[0x5] = &Chip8::OP_5XY0;
     table[0x6] = &Chip8::OP_6XKK;
     table[0x7] = &Chip8::OP_7XKK;
+    table[0x8] = &Chip8::Table8;
+    table[0x9] = &Chip8::OP_9XY0;
+    table[0xA] = &Chip8::OP_ANNN;
+    table[0xB] = &Chip8::OP_BNNN;
+    table[0xC] = &Chip8::OP_CXKK;
+    table[0xD] = &Chip8::OP_DXYN;
+    table[0xE] = &Chip8::TableE;
+    table[0xF] = &Chip8::TableF;
 
+    //table - 0,8 and E
+    for(size_t i = 0; i <= 0xE; i++){
+        table0[i] = &Chip8::OP_NULL;
+        table8[i] = &Chip8::OP_NULL;
+        tableE[i] = &Chip8::OP_NULL;
+    }
+
+    //table0
+    table0[0x0] = &Chip8::OP_00E0;
+    table0[0xE] = &Chip8::OP_00EE;
+
+    //table8
+    table8[0x0] = &Chip8::OP_8XY0;
+    table8[0x1] = &Chip8::OP_8XY1;
+    table8[0x2] = &Chip8::OP_8XY2;
+    table8[0x3] = &Chip8::OP_8XY3;
+    table8[0x4] = &Chip8::OP_8XY4;
+    table8[0x5] = &Chip8::OP_8XY5;
+    table8[0x6] = &Chip8::OP_8XY6;
+    table8[0x7] = &Chip8::OP_8XY7;
+    table8[0xE] = &Chip8::OP_8XYE;
+
+    //tableE
+    tableE[0x1] = &Chip8::OP_EXA1;
+    tableE[0xE] = &Chip8::OP_EX9E;
 }
+
+void Chip8::Table0(){
+    ((*this).*(table0[opcode & 0x000Fu]))();
+}
+
+void Chip8::Table8(){
+    ((*this).*(table8[opcode & 0x000Fu]))();
+}
+
+void Chip8::TableE(){
+    ((*this).*(tableE[opcode & 0x000Fu]))();
+}
+
+void Chip8::TableF(){
+    ((*this).*(tableF[opcode & 0x00FFu]))();
+}
+
+void Chip8::OP_NULL(){}
 
 //clear screen
 void Chip8::OP_00E0(){
